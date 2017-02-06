dab6a02f2902704f39dfa1dd8436dce3023ba2e8
Michel Stuyts <michel.stuyts@gmail.com>
2017-02-06 16:01:08 +0100

    base plugin commit

---
+++ quickcrs_dialog.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ quickcrsDialog
+                                 A QGIS plugin
+ One click to enable your favourite CRS (OTF)
+                             -------------------
+        begin                : 2017-02-06
+        git sha              : $Format:%H$
+        copyright            : (C) 2017 by Michel Stuyts
+        email                : info@stuyts.xyz
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+
+import os
+
+from PyQt4 import QtGui, uic
+
+FORM_CLASS, _ = uic.loadUiType(os.path.join(
+    os.path.dirname(__file__), 'quickcrs_dialog_base.ui'))
+
+
+class quickcrsDialog(QtGui.QDialog, FORM_CLASS):
+    def __init__(self, parent=None):
+        """Constructor."""
+        super(quickcrsDialog, self).__init__(parent)
+        # Set up the user interface from Designer.
+        # After setupUI you can access any designer object by doing
+        # self.<objectname>, and you can use autoconnect slots - see
+        # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
+        # #widgets-and-dialogs-with-auto-connect
+        self.setupUi(self)
