64f3bd371fc70ce6b55cede96f2828f5201597d6
mstuyts <michel.stuyts@gmail.com>
2017-03-02 15:30:08 +0100

    update for NullValue bug

--- QuickCRS/quickcrs.py
+++ QuickCRS/quickcrs.py
@@ -33,7 +33,7 @@ class quickcrs:
         global selectedcrs
         s = QSettings()
         selectedcrs=s.value("quickcrs/crs", "")
-        # Check if the CRS in the settings is an integer. In versions prior to v0.3 of this plugin, the setting contained a text value instead of an integer.
+        # Check if the CRS in the settings is an integer. In version prior to v0.3 of this plugin, the setting contained a text value instead of an integer.
         try:
             testsetting = selectedcrs+1
         except TypeError:
@@ -209,7 +209,7 @@ class quickcrs:
 
     def CrsId2AuthID(self, crsid=0):
         toconvert = QgsCoordinateReferenceSystem()
-        if crsid=="" or crsid==0:
+        if crsid=="" or crsid==0 or crsid is None:
             converted=""
         else:
             toconvert.createFromId( int(crsid), QgsCoordinateReferenceSystem.InternalCrsId )
