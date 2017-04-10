d13c6862a37d908afd127371d41700e439dfe443
mstuyts <michel.stuyts@gmail.com>
2017-04-10 12:03:42 +0200

    updated testfile

--- tests/test_mainfunctionality.py
+++ tests/test_mainfunctionality.py
@@ -1,4 +1,19 @@
+# test for QGIS 2.x
 canvas = iface.mapCanvas()
 target_crs = QgsCoordinateReferenceSystem()
 target_crs.createFromId( 31370, QgsCoordinateReferenceSystem.EpsgCrsId )
 canvas.setDestinationCrs(target_crs)
+
+#test for QGIS 3.x
+target_crs = QgsCoordinateReferenceSystem()
+target_crs.createFromId( 31370, QgsCoordinateReferenceSystem.EpsgCrsId )
+QgsProject.instance().setCrs(target_crs)
+
+# Combined test
+target_crs = QgsCoordinateReferenceSystem()
+target_crs.createFromId( 31370, QgsCoordinateReferenceSystem.EpsgCrsId )
+try:
+    QgsProject.instance().setCrs(target_crs)
+except:
+    canvas = iface.mapCanvas()
+    canvas.setDestinationCrs(target_crs)
